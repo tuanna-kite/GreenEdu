3c9f5c3ed593faa4b453e4406550bcf0659c0d48
Quang Akiyama <quanganhcf2@gmail.com>
2024-06-16 16:42:14 +0700

    update version

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Platform, StyleSheet } from "react-native";
 import { Provider } from "react-redux";
 import store from "./src/store";
 import Root from "./src/navigations/Root";
-import { Box, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { StatusBar } from "react-native";
 import { config } from "./config/gluestack-ui.config";
 
---
+++ src/screens/auth/SplashScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const SplashScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>SplashScreen</Text>
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({})
